7cde1804de8152c30339cca5c469d38b222cff06
sebius77 <gebs.dev@gmail.com>
2022-05-23 18:29:18 +0200

    update

--- DependencyInjection/Sebius77CasAuthExtension.php
+++ DependencyInjection/Sebius77CasAuthExtension.php
@@ -5,24 +5,23 @@ namespace Sebius77\CasBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  * https://symfony.com/doc/current/bundles/extension.html
  */
-class CasAuthExtension extends Extension
+class Sebius77CasAuthExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $authenticator = $container->autowire('cas_authenticator',
+        $authenticator = $container->autowire('sebius77.cas_authenticator',
             'Sebius77\CasBundle\Security\CasAuthenticator');
         $authenticator->setArguments(array($config));
 
-        $container->register('cas_user_provider',
+        $container->register('sebius77.cas_user_provider',
             'Sebius77\CasBundle\Security\User\CasUserProvider');
     }
 }
