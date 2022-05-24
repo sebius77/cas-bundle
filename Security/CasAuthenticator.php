ba48c0765c67cc553d219f474a55d172fea5a402
sebius77 <gebs.dev@gmail.com>
2022-05-24 12:09:56 +0200

    update

--- Security/CasAuthenticator.php
+++ Security/CasAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -68,7 +69,7 @@ class CasAuthenticator extends AbstractAuthenticator
         if (isset($xml->authenticationSuccess)) {
             return new SelfValidatingPassport(new UserBadge($xml));
         }
-        return [];
+        throw new CustomUserMessageAuthenticationException('Erreur rencontrée.....');
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?RedirectResponse
