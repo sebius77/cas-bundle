0e56ee92cd13dec8369295b762cc29f585f01dcd
sebius77 <gebs.dev@gmail.com>
2022-05-25 11:07:22 +0200

    update Configuration

--- Security/User/CasUser.php
+++ Security/User/CasUser.php
@@ -24,10 +24,7 @@ class CasUser implements UserInterface
 
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+       return $this->roles;
     }
 
     public function setRoles(array $roles): self
