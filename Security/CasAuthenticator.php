636c76bb8e7339baddf4c6700c93659726b8e34f
Sébastien Gaudin <sebastien.gaudin@universite-paris-saclay.fr>
2026-02-13 10:36:18 +0100

    update

--- Security/CasAuthenticator.php
+++ Security/CasAuthenticator.php
@@ -1,5 +1,20 @@
 <?php
-// Sebius77/CasBundle/Security/CasAuthenticator.php
+/*
+      Copyright 2026 CAS-BUNDLE - Sébastien Gaudin (sebastien.gaudin10@gmail.com)
+      
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+      
+          http://www.apache.org/licenses/LICENSE-2.0
+      
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+*/
+
 namespace Sebius77\CasBundle\Security;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -52,13 +67,13 @@ class CasAuthenticator extends AbstractAuthenticator
      */
     public function supports(Request $request): ?bool
     {
-        return (bool) $request->get($this->query_ticket_parameter);
+        return (bool) $request($this->query_ticket_parameter);
     }
 
     public function authenticate(Request $request): Passport
     {
         $url = $this->server_validation_url . '?' . $this->query_ticket_parameter . '=' .
-            $request->get($this->query_ticket_parameter) . '&' .
+            $request($this->query_ticket_parameter) . '&' .
             $this->query_service_parameter . '=' . urlencode($this->removeCasTicket($request->getUri()));
 
         $response = $this->client->request('GET', $url, $this->options);
